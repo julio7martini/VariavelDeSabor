7c2bf867d641fd10eefc927852fd7333ac40d9ab
lucas.bretzke <lucas.bretzke@BUE002D014>
2025-01-20 21:52:35 -0300

    initiate: stock entity

--- AplicacaoWebCantina/Models/Estoque/Estoque.cs
+++ AplicacaoWebCantina/Models/Estoque/Estoque.cs
@@ -2,5 +2,10 @@
 {
     public class Estoque
     {
+        public int Id { get; set; }
+        public int ProdutoId { get; set; } // Referência ao Produto
+        public int QuantidadeAtual { get; set; }
+        public int QuantidadeMinima { get; set; } // Quantidade mínima para alerta de reposição
+        public DateTime DataUltimaReposicao { get; set; }
     }
 }
