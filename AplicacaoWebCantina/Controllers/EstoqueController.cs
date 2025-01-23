d1ce6816e1dfad2c13cb5fef0bad865472cbdce8
lucas.bretzke <lucas.bretzke@BUE002D014>
2025-01-22 21:57:47 -0300

    first commit

--- AplicacaoWebCantina/Controllers/EstoqueController.cs
+++ AplicacaoWebCantina/Controllers/EstoqueController.cs
@@ -11,17 +11,28 @@ namespace AplicacaoWebCantina.Controllers
     {
         // Dados mocados
         private static List<EstoqueModel> Estoques = new List<EstoqueModel>
-{
-             new EstoqueModel { Id = 1, ProdutoId = 101, QuantidadeAtual = 50, QuantidadeMinima = 10, DataUltimaReposicao = DateTime.Now.AddMonths(-1) },
-             new EstoqueModel { Id = 2, ProdutoId = 102, QuantidadeAtual = 5, QuantidadeMinima = 10, DataUltimaReposicao = DateTime.Now.AddMonths(-2) }
-};
+        {
+            new EstoqueModel { Id = 1, ProdutoId = 101, QuantidadeAtual = 50, QuantidadeMinima = 10, DataUltimaReposicao = DateTime.Now.AddMonths(-1) },
+            new EstoqueModel { Id = 2, ProdutoId = 102, QuantidadeAtual = 5, QuantidadeMinima = 10, DataUltimaReposicao = DateTime.Now.AddMonths(-2) },
+            new EstoqueModel { Id = 3, ProdutoId = 103, QuantidadeAtual = 30, QuantidadeMinima = 15, DataUltimaReposicao = DateTime.Now.AddMonths(-3) },
+            new EstoqueModel { Id = 4, ProdutoId = 104, QuantidadeAtual = 12, QuantidadeMinima = 8, DataUltimaReposicao = DateTime.Now.AddMonths(-4) },
+            new EstoqueModel { Id = 5, ProdutoId = 105, QuantidadeAtual = 20, QuantidadeMinima = 5, DataUltimaReposicao = DateTime.Now.AddMonths(-5) },
+            new EstoqueModel { Id = 6, ProdutoId = 106, QuantidadeAtual = 45, QuantidadeMinima = 20, DataUltimaReposicao = DateTime.Now.AddMonths(-6) },
+            new EstoqueModel { Id = 7, ProdutoId = 107, QuantidadeAtual = 25, QuantidadeMinima = 10, DataUltimaReposicao = DateTime.Now.AddMonths(-7) }
+        };
 
         // Mostrar a View do estoque
         public IActionResult Index()
         {
+            // Associando produtos fictícios aos estoques
             foreach (var estoque in Estoques)
             {
-                estoque.Produto = new ProdutoModel { ID = estoque.ProdutoId, Nome = "Produto " + estoque.ProdutoId };
+                estoque.Produto = new ProdutoModel
+                {
+                    ID = estoque.ProdutoId,
+                    Nome = "Produto " + estoque.ProdutoId,
+                    Preco = (double)(10.00m + estoque.ProdutoId)  
+                };
             }
 
             return View(Estoques);
@@ -33,14 +44,13 @@ namespace AplicacaoWebCantina.Controllers
             if (estoque == null)
                 return NotFound();
 
-            var produto = new ProdutoModel { ID = estoque.ProdutoId, Nome = "Produto " + estoque.ProdutoId };
-
-            estoque.Produto = produto;  
+            var produto = new ProdutoModel { ID = estoque.ProdutoId, Nome = "Produto " + estoque.ProdutoId, Preco = (double)(10.00m + estoque.ProdutoId) };
+            estoque.Produto = produto;
 
             return View(estoque);
         }
 
-
+        // Processar a edição do estoque
         [HttpPost]
         public IActionResult Edit(EstoqueModel estoque)
         {
@@ -50,10 +60,14 @@ namespace AplicacaoWebCantina.Controllers
                 if (estoqueExistente != null)
                 {
                     estoqueExistente.QuantidadeAtual = estoque.QuantidadeAtual;
+                    estoqueExistente.QuantidadeMinima = estoque.QuantidadeMinima;
                     estoqueExistente.DataUltimaReposicao = estoque.DataUltimaReposicao;
+
+                    return RedirectToAction(nameof(Index));
                 }
-                return RedirectToAction(nameof(Index));
             }
+
+            // Se o modelo não for válido, exibir a tela de edição novamente
             return View(estoque);
         }
     }
