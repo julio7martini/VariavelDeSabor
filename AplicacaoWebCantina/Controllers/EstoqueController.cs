c747328070f205daf7bd6f932195b62ff8acd2ae
lucas.bretzke <lucas.bretzke@BUE002D015>
2025-01-27 19:38:56 -0300

    fix(EstoqueController): edit RedirectToAction("Index")

--- AplicacaoWebCantina/Controllers/EstoqueController.cs
+++ AplicacaoWebCantina/Controllers/EstoqueController.cs
@@ -1,6 +1,9 @@
 ﻿using AplicacaoWebCantina.Models.Estoque;
 using AplicacaoWebCantina.Models.Produto;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AplicacaoWebCantina.Controllers
 {
@@ -8,27 +11,17 @@ namespace AplicacaoWebCantina.Controllers
     {
         // Dados mocados
         private static List<EstoqueModel> Estoques = new List<EstoqueModel>
-        {
-            new EstoqueModel { ID = 1, ProdutoId = 101, QuantidadeAtual = 50, QuantidadeMinima = 10, DataUltimaReposicao = DateTime.Now.AddMonths(-1) },
-            new EstoqueModel { ID = 2, ProdutoId = 102, QuantidadeAtual = 5, QuantidadeMinima = 10, DataUltimaReposicao = DateTime.Now.AddMonths(-2) },
-            new EstoqueModel { ID = 3, ProdutoId = 103, QuantidadeAtual = 30, QuantidadeMinima = 15, DataUltimaReposicao = DateTime.Now.AddMonths(-3) },
-            new EstoqueModel { ID = 4, ProdutoId = 104, QuantidadeAtual = 12, QuantidadeMinima = 8, DataUltimaReposicao = DateTime.Now.AddMonths(-4) },
-            new EstoqueModel { ID = 5, ProdutoId = 105, QuantidadeAtual = 20, QuantidadeMinima = 5, DataUltimaReposicao = DateTime.Now.AddMonths(-5) },
-            new EstoqueModel { ID = 6, ProdutoId = 106, QuantidadeAtual = 45, QuantidadeMinima = 20, DataUltimaReposicao = DateTime.Now.AddMonths(-6) },
-            new EstoqueModel { ID = 7, ProdutoId = 107, QuantidadeAtual = 25, QuantidadeMinima = 10, DataUltimaReposicao = DateTime.Now.AddMonths(-7) }
-        };
+{
+             new EstoqueModel { Id = 1, ProdutoId = 101, QuantidadeAtual = 50, QuantidadeMinima = 10, DataUltimaReposicao = DateTime.Now.AddMonths(-1) },
+             new EstoqueModel { Id = 2, ProdutoId = 102, QuantidadeAtual = 5, QuantidadeMinima = 10, DataUltimaReposicao = DateTime.Now.AddMonths(-2) }
+};
 
+        // Mostrar a View do estoque
         public IActionResult Index()
         {
-            // Associando produtos fictícios aos estoques
             foreach (var estoque in Estoques)
             {
-                estoque.Produto = new ProdutoModel
-                {
-                    ID = estoque.ProdutoId,
-                    Nome = "Produto " + estoque.ProdutoId,
-                    Preco = (double)(10.00m + estoque.ProdutoId)  
-                };
+                estoque.Produto = new ProdutoModel { ID = estoque.ProdutoId, Nome = "Produto " + estoque.ProdutoId };
             }
 
             return View(Estoques);
@@ -36,30 +29,28 @@ namespace AplicacaoWebCantina.Controllers
 
         public IActionResult Edit(int id)
         {
-            var estoque = Estoques.FirstOrDefault(e => e.ID == id);
+            var estoque = Estoques.FirstOrDefault(e => e.Id == id);
             if (estoque == null)
                 return NotFound();
 
-            var produto = new ProdutoModel { ID = estoque.ProdutoId, Nome = "Produto " + estoque.ProdutoId, Preco = (double)(10.00m + estoque.ProdutoId) };
-            estoque.Produto = produto;
+            var produto = new ProdutoModel { ID = estoque.ProdutoId, Nome = "Produto " + estoque.ProdutoId };
+
+            estoque.Produto = produto;  
 
             return View(estoque);
         }
 
+
         [HttpPost]
         public IActionResult Edit(EstoqueModel estoque)
         {
-                var estoqueExistente = Estoques.FirstOrDefault(i => i.ID == estoque.ID);
+                var estoqueExistente = Estoques.FirstOrDefault(e => e.Id == estoque.Id);
                 if (estoqueExistente != null)
                 {
                     estoqueExistente.QuantidadeAtual = estoque.QuantidadeAtual;
-                    estoqueExistente.QuantidadeMinima = estoque.QuantidadeMinima;
                     estoqueExistente.DataUltimaReposicao = estoque.DataUltimaReposicao;
-
                 }
-                     return RedirectToAction("Index");
+                return RedirectToAction("Index");
         }
-
-
     }
 }
