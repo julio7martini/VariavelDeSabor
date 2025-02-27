2b878c25f310fe1f3768bcf11d64ccef36011e65
PedroDeMiranda <pedromirandaneto.2006@gmail.com>
2025-02-27 19:09:00 -0300

    Adicionando Controller de Ensino

---
+++ AplicacaoWebCantina/Controllers/BolinhasController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MinhaPrimeiraAplicacaoWeb.Controllers
+{
+    public class Bolinha
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+    }
+
+    public class BolinhasController : Controller
+    {
+        private static List<Bolinha> _Bolinhas = new List<Bolinha>();
+
+        [HttpGet("api/v1/bolinhas")]
+        public IActionResult Get()
+        {
+            return Ok(_Bolinhas);
+        }
+
+        [HttpPost("api/v1/bolinha")]
+        public IActionResult Post([FromBody] Bolinha bolinha)
+        {
+            bolinha.Id = Guid.NewGuid();
+            _Bolinhas.Add(bolinha);
+
+            Thread.Sleep(5000);
+
+            return Ok(bolinha.Id);
+        }
+
+        [HttpDelete("api/v1/bolinha/{idBolinha}")]
+        public IActionResult Delete(Guid idBolinha)
+        {
+            _Bolinhas.RemoveAll(bolinha => bolinha.Id == idBolinha); //LINQ
+
+            return Ok("Registro excluído!");
+        }
+    }
+}
